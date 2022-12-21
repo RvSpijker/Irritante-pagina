850c02bd0389d265e41798a7f33db06f7d736ac2
RvSpijker <r.vanspijker@student.alfa-college.nl>
2022-12-21 11:58:24 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+    while (1 == 1) {
+        random_bg_color();
+      }
+   
