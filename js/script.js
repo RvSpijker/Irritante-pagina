179ab0d1494b1d77d6f83276e54d5201f1c0e948
Kansloos12345 <108752066+Kansloos12345@users.noreply.github.com>
2023-01-11 14:18:24 +0100

    lol
    lol

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ function random_bg_color() {
 // while(true) {
 //    alert("JE MOEDER IS LELIJK HAHAHAHAHAHAHAHAHAHA");
 // }
+
+const movingElement = document.getElementById("rat");
+let x = 0;
+let y = 0;
+let xSpeed = 2.5;
+let ySpeed = 3.5;
+
+function updatePosition() {
+    x += xSpeed;
+    y += ySpeed;
+    movingElement.style.transform = `translate(${x}px, ${y}px)`;
+
+    // Check for screen boundaries and change direction 
+    if (x + movingElement.clientWidth > window.innerWidth || x < 0) {
+        xSpeed = -xSpeed;
+    }
+    if (y + movingElement.clientHeight > window.innerHeight || y < 0) {
+        ySpeed = -ySpeed;
+    }
+    requestAnimationFrame(updatePosition);
+}
+
+//start animation
+requestAnimationFrame(updatePosition);
