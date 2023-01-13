ec33f6671907bf4a382569f75ca0f528bff4ac53
RvSpijker <r.vanspijker@student.alfa-college.nl>
2023-01-13 12:11:23 +0100

    NUKE ヾ(•ω•`)o

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// random size image
 setInterval(function(){
     var gifs1 = document.getElementsByClassName("gifs1");
     for(var i = 0; i < gifs1.length; i++) {
@@ -6,8 +7,18 @@ setInterval(function(){
     }
 }, 500);
 
-
+// cross that removes image
 function removeDiv(divId) {
     var div = document.getElementById(divId);
     div.parentNode.removeChild(div);
   }
+
+// remove div1 to div40
+function removeDivs() {
+    for (let i = 1; i <= 40; i++) {
+        let div = document.getElementById(`div${i}`);
+        if (div) {
+            div.parentNode.removeChild(div);
+        }
+    }
+}
