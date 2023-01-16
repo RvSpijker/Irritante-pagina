cbe614caac318c77de9ca2e181d44e3ed21f464e
RvSpijker <r.vanspijker@student.alfa-college.nl>
2023-01-16 13:31:37 +0100

    blahaj popup

--- js/script.js
+++ js/script.js
@@ -53,3 +53,16 @@ requestAnimationFrame(updatePosition);
 window.onbeforeunload = function() {
     return "Refreshing the page is not allowed.";
 };
+
+function random_3(){
+    var myrandom=Math.round(Math.random()*2)
+    var link1="http://www.codingforums.com"
+    var link2="http://www.cssdrive.com"
+    var link3="http://www.dynamicdrive.com"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+}
